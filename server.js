f587d09530fb6f89cf90ef17bee07588a0a9ee0d
Kenneth Landis <Eightfletcher@msn.com>
2021-05-29 20:07:12 -0500

    wireframed Auth and security
    Added auth validation with UUID Token for testing as well as cors/helmet security measures.

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet')
+const cors = require('cors')
 const MOVIES = require('./movies.json')
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
 
-app.use((req, res) => {
-    res.send('Successful test')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    next();
+
+})
+
+app.get('/movie', function getMovies(req, res) {
+    response = MOVIES
+
+    res.json(response)
 })
 
 const PORT = 8000;
 
 app.listen(PORT, () => {
-    console.log('Server listening at port 8000')
+    console.log(`Server listening at PORT ${PORT}`)
 })
